8bb5f662eeb3072183e16add83335b3e714e2416
serhatkoyuncu <serhat_koyuncu@icloud.com>
2021-12-29 22:47:15 +0300

    last

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://django-blog-case.herokuapp.com/","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["django-blog-case.herokuapp.com","localhost","127.0.0.1"]
 
 
 # Application definition
